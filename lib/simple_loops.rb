d3e2276347cef1b67ea2aeba852dd184afa500e2
adventurous-format-6947 <bminczu@gmail.com>
2020-10-05 19:45:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -10,7 +10,7 @@ def loop_message_n_times(message,number)
 number.times do
   puts message
 end
-end 
+end
 
 def output_array(array)
   counter=0
@@ -18,6 +18,6 @@ def output_array(array)
     puts array[counter]
     counter+=1
   end
-    
+
   end
 end

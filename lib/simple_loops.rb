b4ddf2aeefab3ccd17d60d3d4e932b8921cc01fb
adventurous-format-6947 <bminczu@gmail.com>
2020-10-05 19:10:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,12 @@
-# Write your methods here
+def loop_message_five_times(message)
+puts message
+puts message
+puts message
+puts message
+puts message
+end
+
+def loop_message_five_times(message)
+  5.times do
+    message
+end

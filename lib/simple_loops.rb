38a9cdb63e32553d87d8797d0104120a9786cdc6
adventurous-format-6947 <bminczu@gmail.com>
2020-10-05 19:53:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -20,10 +20,11 @@ def output_array(array)
   end
   end
 
-def return_string_array(array)
-  count=0
-  while count < array.length do
-    array[count].to_s
-    count +=1
+  def return_string_array(array)
+    count = 0
+    while count < array.length do
+      array[count] = array[count].to_s
+      count += 1
+    end
+    array
   end
-end
